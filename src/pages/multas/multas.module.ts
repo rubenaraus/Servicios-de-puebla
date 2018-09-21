7ad17e0ac035de4ba81855ce5fde24eb55ba0687
Admin <admin@MacBookRuben.local>
2018-09-21 13:41:56 -0500

    menu inicial

---
+++ src/pages/multas/multas.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MultasPage } from './multas';
+
+@NgModule({
+  declarations: [
+    MultasPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(MultasPage),
+  ],
+})
+export class MultasPageModule {}
